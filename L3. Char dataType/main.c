8391b54ebd2ee234f15a0a672b089339d4fd053a
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-05-28 06:46:08 +0700

    Implement code changes to enhance functionality and improve performance

---
+++ L3. Char dataType/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    char c = 'A'; // Character data type
+    char str[] = "Hello, World!"; // String data type
+    printf("Character: %c\n", c); // Print character
+    printf("String: %s\n", str); // Print string
+}
