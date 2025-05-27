3f76ac1c1d102fd58585a63127c6d5942e4fcf55
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-05-28 06:32:35 +0700

    Refactor code structure for improved readability and maintainability

---
+++ L2.Escape Sequence's in C Explained/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    // Using escape sequences to print special characters
+    printf("Hello, World!\n"); // New line
+    printf("This is a tab:\tTabbed text\n"); // Tab character
+    printf("This is a backslash: \\\n"); // Backslash
+    printf("This is a single quote: '\n"); // Single quote
+    printf("This is a double quote: \"\n"); // Double quote
+    printf("This is a carriage return:\rOverwritten text\n"); // Carriage return
+
+    return 0;
+}   
