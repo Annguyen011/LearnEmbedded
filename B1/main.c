aee0a755065214bf0f697975cb982e687eb80360
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-25 21:05:10 +0700

    Refactor launch configuration and enhance task definitions for improved build process

--- B1/main.c
+++ B1/main.c
@@ -2,5 +2,10 @@
 
 int main() {
     printf("Hello, World!\n");
+    printf("This is a simple C program.\n");
+    printf("It prints messages to the console.\n");
+    int a = 100;
+    printf("%d",a);
+
     return 0;
 }
