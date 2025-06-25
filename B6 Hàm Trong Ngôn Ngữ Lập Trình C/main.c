e9f5dc4edcc8b86876da14724162c8181d762480
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-26 05:54:55 +0700

    Add functionality to check for perfect squares and update main function

--- B6 Hàm Trong Ngôn Ngữ Lập Trình C/main.c
+++ B6 Hàm Trong Ngôn Ngữ Lập Trình C/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 void printMessage() {
     printf("Hello from the function!\n");
 }
 
+int chinhPhuong(int  n)
+{
+    int can = sqrt(n);
+    if(can * can == n)
+        return 1; // n is a perfect square
+    else
+        return 0; // n is not a perfect square
+}
+
+
 int main() {
     printMessage();
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (chinhPhuong(number))
+        printf("%d is a perfect square.\n", number);
+    else
+        printf("%d is not a perfect square.\n", number);
     return 0;
 }
