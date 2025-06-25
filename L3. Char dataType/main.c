045d55668d71d5b2b0a4afb006fccded87b335a3
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-25 07:50:08 +0700

    Remove obsolete files related to binary conversion, escape sequences, char data type, and ARM architecture

--- L3. Char dataType/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    char c = 'A'; // Character data type
-    char str[] = "Hello, World!"; // String data type
-    printf("Character: %c\n", c); // Print character
-    printf("String: %s\n", str); // Print string
-}
