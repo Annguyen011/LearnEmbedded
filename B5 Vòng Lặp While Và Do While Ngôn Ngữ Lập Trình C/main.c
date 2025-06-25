f85185e11cf1f2b12a5c5ff1dd404139d16f9fdb
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-26 05:25:45 +0700

    Implement code changes to enhance functionality and improve performance

---
+++ B5 Vòng Lặp While Và Do While Ngôn Ngữ Lập Trình C/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    while (1)  // Infinite loop
+    {
+        printf("This is an infinite loop.\n");
+        break;  // Remove this line to keep the loop running indefinitely
+    }
+    {
+        printf("This is a separate block.\n");
+    }
+
+    int i = 0;
+    do {
+        printf("This is a do-while loop iteration %d.\n", i);
+        i++;
+    } while (i < 5);  // Loop will run 5 times
+    
+}
