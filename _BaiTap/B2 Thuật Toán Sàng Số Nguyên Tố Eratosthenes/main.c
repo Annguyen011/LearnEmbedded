6e0e5f724b042b78b530e2a5c7904fb1f1326303
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-26 08:54:24 +0700

    Refactor code structure for improved readability and maintainability

---
+++ _BaiTap/B2 Thuật Toán Sàng Số Nguyên Tố Eratosthenes/main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+
+#define MAX 1000000
+int isPrime[MAX + 1];
+void sieveOfEratosthenes() {
+    for (int i = 0; i <= MAX; i++) {
+        isPrime[i] = 1; // Assume all numbers are prime
+    }
+    isPrime[0] = isPrime[1] = 0; // 0 and 1 are not prime numbers
+
+    for (int i = 2; i * i <= sqrt(MAX); i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= MAX; j += i) {
+                isPrime[j] = 0; // Mark multiples of i as non-prime
+            }
+        }
+    }
+}
+
+int main() {
+    sieveOfEratosthenes();
+    
+    int n;
+    printf("Enter a number to check if it is prime: ");
+    scanf("%d", &n);
+    
+    if (n < 0 || n > MAX) {
+        printf("Number out of range. Please enter a number between 0 and %d.\n", MAX);
+        return 1;
+    }
+    
+    if (isPrime[n]) {
+        printf("%d is a prime number.\n", n);
+    } else {
+        printf("%d is not a prime number.\n", n);
+    }
+    
+    return 0;
+}
