f53924cde495c3b852a0528565bae38cb47a6bb6
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-27 15:31:42 +0700

    Refactor code structure for improved readability and maintainability

--- 1_LyThuyet/B005/main.cpp
+++ 1_LyThuyet/B005/main.cpp
@@ -22,6 +22,31 @@ using namespace std;
 //#endregion
 
 int main() {
-    // TODO: Viết code ở đây
+    
+    // Cac kieu du lieu co ban
+    int a = 10; // Kieu so nguyen
+    double b = 3.14; // Kieu so thuc
+    char c = 'A'; // Kieu ky tu
+    string d = "Hello"; // Kieu chuoi
+    bool e = true; // Kieu logic
+    // Cac kieu du lieu cau truc
+    vector<int> vec = {1, 2, 3, 4, 5}; // Vector
+    pair<int, string> p = {1, "One"}; // Pair
+    map<int, string> m; // Map
+    m[1] = "One"; // Them phan tu vao map
+    set<int> s = {1, 2, 3}; // Set
+    unordered_map<int, string> um; // Unordered Map
+    um[1] = "One"; // Them phan tu vao unordered map
+    unordered_set<int> us = {1, 2, 3}; // Unordered Set
+    stack<int> st; // Stack
+    st.push(1); // Them phan tu vao stack
+    queue<int> q; // Queue
+    q.push(1); // Them phan tu vao queue
+    deque<int> deq; // Deque
+    deq.push_back(1); // Them phan tu vao cuoi deque
+    bitset<8> bs; // Bitset
+    bs.set(0); // Dat bit thu nhat
+    bs.set(1); // Dat bit thu hai
+    bs.reset(0); // Dat lai bit thu nhat ve 0
     return 0;
 }
--- 1_LyThuyet/B006/main.cpp
+++ 1_LyThuyet/B006/main.cpp
@@ -22,6 +22,43 @@ using namespace std;
 //#endregion
 
 int main() {
-    // TODO: Viết code ở đây
+    // Bien cuc bo
+    int n;
+    // Nhap so nguyen n
+    cout << "Nhap so nguyen n: ";
+    cin >> n;
+    // Kiem tra n la so chan hay le
+    if (n % 2 == 0) {
+        cout << n << " la so chan." << endl;
+    } else {
+        cout << n << " la so le." << endl;
+    }
+    // Kiem tra n la so nguyen to
+    bool isPrime = true;
+    if (n < 2) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime) {
+        cout << n << " la so nguyen to." << endl;
+    } else {
+        cout << n << " khong phai la so nguyen to." << endl;
+    }
+    // Vi du ve biến cục bô
+    int a = 5, b = 10;
+    cout << "Bien cuc bo a: " << a << ", b: " << b << endl;
+    // Thay doi gia tri cua bien a
+    a = 15;
+    cout << "Sau khi thay doi, bien cuc bo a: " << a << ", b: " << b << endl;
+    // Thay doi gia tri cua bien b
+    b = 20;
+    cout << "Sau khi thay doi, bien cuc bo a: " << a << ", b: " << b << endl;
+    
     return 0;
 }
