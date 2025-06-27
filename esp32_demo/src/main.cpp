d04ec41ccd22bb5f2c87fca8006fd590325276f7
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-27 21:01:47 +0700

    Refactor main.cpp to simplify LED control setup and remove unused function declarations

--- esp32_demo/src/main.cpp
+++ esp32_demo/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
+// tạp ra code để cho led có san trên esp32 nhấp nháy theo thời gian
 
-// put function declarations here:
-int myFunction(int, int);
+// Đèn LED gắn sẵn trên ESP32 thường là GPIO 2
+const int ledPin = 2;  
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);  // Cấu hình chân 2 là output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("LED is ON");
 }
