eaf856b70f50f9202dcc203d37662c251ccd99e5
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-27 07:06:41 +0700

    Refactor main.cpp to move printMessage function definition above main and clean up commented code

--- 1_LyThuyet/B1/main.cpp
+++ 1_LyThuyet/B1/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int	main(int argc, char **argv)
+void printMessage(const string &message)
 {
     cout << "Hello World!" << endl;
     cout << "This is my first C++ program." << endl;
@@ -10,11 +10,16 @@ int	main(int argc, char **argv)
     int b = 10;
     int sum = a + b;
     cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
-    int c;
-    cin >> c;
-    cout << "You entered: " << c << endl;
+    // int c;
+    // cin >> c;
+    // cout << "You entered: " << c << endl;
     // cout << "Press any key to continue..." << endl;
     // Wait for user input before closing the console window
     system("pause");
+}
+
+int	main(int argc, char **argv)
+{
+    
     return 0;
 }
