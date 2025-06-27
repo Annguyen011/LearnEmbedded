f8c94f8267268e7e3ff76c54c41c3ca8d3f95446
Annguyen011 <nguyenvanan01102003@gmail.com>
2025-06-27 15:31:38 +0700

    Add initial implementation for B003 to B040 with standard C++ library includes and main function template

--- 1_LyThuyet/B002/main.cpp
+++ 1_LyThuyet/B002/main.cpp
@@ -1,17 +1,27 @@
-#pragma region LIBRARIES
-
-#include<algorithm>
-#include<iostream>
-#include<iomanip>
+//#region Thư viện C++ chuẩn
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cctype>
+#include <cstring>
 using namespace std;
+//#endregion
 
-#pragma endregion
-
-
-
-int main()
-{
-
-
-    return 1;
+int main() {
+    // TODO: Viết code ở đây
+    return 0;
 }
--- 1_LyThuyet/B010/main.cpp
+++ 1_LyThuyet/B010/main.cpp
@@ -1,12 +1,27 @@
-#include<iostream>
-#include<algorithm>
+//#region Thư viện C++ chuẩn
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cctype>
+#include <cstring>
 using namespace std;
+//#endregion
 
-int int	main(int argc, char **argv)
-{
-    
-
-
-    system("pause");
+int main() {
+    // TODO: Viết code ở đây
     return 0;
 }
